dd78afd46aa2ed192d7cfa8af256fd8518704ecf
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2024-04-05 09:49:57 +0200

    add oidc mocked and upgrade dep

--- src/oidc.tsx
+++ src/oidc.tsx
@@ -1,12 +1,16 @@
 import { createReactOidc } from 'oidc-spa/react'
+import { createMockReactOidc } from 'oidc-spa/mock/react'
 import { z } from 'zod'
 
-export const { OidcProvider, useOidc, prOidc } = createReactOidc({
-  clientId: import.meta.env.VITE_OIDC_CLIENT_ID,
-  issuerUri: import.meta.env.VITE_OIDC_ISSUER,
-  publicUrl: import.meta.env.BASE_URL,
-  decodedIdTokenSchema: z.object({
-    sub: z.string(),
-    preferred_username: z.string(),
-  }),
-})
+export const { OidcProvider, useOidc, prOidc } =
+  import.meta.env.VITE_OIDC_ENABLED === 'false'
+    ? createMockReactOidc({ isUserInitiallyLoggedIn: false })
+    : createReactOidc({
+        clientId: import.meta.env.VITE_OIDC_CLIENT_ID,
+        issuerUri: import.meta.env.VITE_OIDC_ISSUER,
+        publicUrl: import.meta.env.BASE_URL,
+        decodedIdTokenSchema: z.object({
+          sub: z.string(),
+          preferred_username: z.string(),
+        }),
+      })
