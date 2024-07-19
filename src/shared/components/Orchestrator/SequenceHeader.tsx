5be53370498e89ecbfa3f9a067f20c5485b4bc2c
Dylan Decrulle <dylan.decrulle@outlook.fr>
2024-07-19 12:25:21 +0200

    style: improve Sequence header style

--- src/shared/components/Orchestrator/SequenceHeader.tsx
+++ src/shared/components/Orchestrator/SequenceHeader.tsx
@@ -27,21 +27,20 @@ export function SequenceHeader(props: SequenceHeaderProps) {
 
   if (pagination === 'question')
     return (
-      <div>
+      <div className={fr.cx('fr-mt-1w')}>
         <h2 className={fr.cx('fr-stepper__title')}>{currentSequence.label}</h2>
       </div>
     )
 
   return (
     <div className={fr.cx('fr-stepper', 'fr-mb-0')}>
-      <div className={fr.cx('fr-stepper__title')}>
-        <h2>{currentSequence.label}</h2>
+      <h2 className={fr.cx('fr-stepper__title', 'fr-mb-0')}>
+        {currentSequence.label}
         <span className={fr.cx('fr-stepper__state')}>
           {t('stepper state', { currentStep, stepCount })}
         </span>
-      </div>
-      <p>{currentSequence.description}</p>
-
+      </h2>
+      {currentSequence.description && <p>{currentSequence.description}</p>}
       <div
         className={fr.cx('fr-stepper__steps')}
         data-fr-current-step={currentStep}
--- src/shared/components/Orchestrator/SurveyContainer.tsx
+++ src/shared/components/Orchestrator/SurveyContainer.tsx
@@ -48,23 +48,57 @@ export function SurveyContainer(
   return (
     <>
       {!isPreviousButtonDisplayed && (
-        <div className={fr.cx('fr-grid-row', 'fr-mt-1w')}>
+        <>
           <div className={fr.cx('fr-container')}>
             {displaySequenceHeader && (
               <SequenceHeader overview={overview} pagination={pagination} />
             )}
-            <Button
-              id="button-precedent"
-              title={t('button previous title')}
-              priority="tertiary no outline"
-              iconId="fr-icon-arrow-left-line"
-              onClick={handlePreviousClick}
-              disabled={isPreviousButtonDisplayed}
-            >
-              {t('button previous label')}
-            </Button>
+            <div className={fr.cx('fr-grid-row', 'fr-grid-row--center')}>
+              <div className={fr.cx('fr-col', 'fr-grid-row--left')}>
+                <Button
+                  id="button-precedent"
+                  title={t('button previous title')}
+                  priority="tertiary no outline"
+                  iconId="fr-icon-arrow-left-line"
+                  onClick={handlePreviousClick}
+                  disabled={isPreviousButtonDisplayed}
+                >
+                  {t('button previous label')}
+                </Button>
+              </div>
+
+              {pagination === 'sequence' && currentPage === 'lunaticPage' && (
+                <>
+                  <div
+                    className={fr.cx(
+                      'fr-col',
+                      ...(isLayoutExpanded
+                        ? (['fr-grid-row--right'] as const)
+                        : (['fr-grid-row--left'] as const)),
+                      'fr-hidden',
+                      'fr-unhidden-md',
+                      'fr-col-offset-8',
+                      'fr-col-offset-md-9'
+                    )}
+                  >
+                    <Button
+                      iconId={
+                        isLayoutExpanded
+                          ? 'ri-collapse-diagonal-line'
+                          : 'ri-expand-diagonal-line'
+                      }
+                      priority="tertiary"
+                      onClick={() =>
+                        setIsLayoutExpanded((expanded) => !expanded)
+                      }
+                      title={t('button expand')}
+                    />
+                  </div>
+                </>
+              )}
+            </div>
           </div>
-        </div>
+        </>
       )}
 
       <div className={fr.cx('fr-container')}>
@@ -78,23 +112,6 @@ export function SurveyContainer(
                 : [])
             )}
           >
-            {pagination === 'sequence' && currentPage === 'lunaticPage' && (
-              <div
-                className={fr.cx('fr-hidden', 'fr-unhidden-md')}
-                style={{ justifyContent: 'flex-end', textAlign: 'right' }}
-              >
-                <Button
-                  iconId={
-                    isLayoutExpanded
-                      ? 'ri-collapse-diagonal-line'
-                      : 'ri-expand-diagonal-line'
-                  }
-                  priority="tertiary"
-                  onClick={() => setIsLayoutExpanded((expanded) => !expanded)}
-                  title={t('button expand')}
-                />
-              </div>
-            )}
             {children}
             <Button
               priority="primary"
