61980846c252d634f47988e217aeeaa6945da759
Quentin Ruhier <99256289+QRuhier@users.noreply.github.com>
2025-02-19 14:21:07 +0100

    chore(deps): lunatic 3.4.17 & lunatic-dsfr 2.4.7 (#194)

--- src/components/orchestrator/utils/data.ts
+++ src/components/orchestrator/utils/data.ts
@@ -1,11 +1,28 @@
 import type { LunaticData } from '@inseefr/lunatic'
 
 /**
+ * Lunatic removed EDITED, FORCED, INPUTTED, PREVIOUS from its data type,
+ * but those states still can be in the data for "old" questionnaires, for now we recreate it.
+ */
+type ExtendedCollectedData =
+  | Record<
+      string,
+      Partial<{
+        COLLECTED: unknown
+        EDITED?: unknown
+        FORCED?: unknown
+        INPUTTED?: unknown
+        PREVIOUS?: unknown
+      }>
+    >
+  | undefined
+
+/**
  * Remove useless variables to reduce payload size in API calls
  * (i.e. everything except COLLECTED)
  */
 export function trimCollectedData(
-  data: LunaticData['COLLECTED'],
+  data: ExtendedCollectedData,
 ): LunaticData['COLLECTED'] {
   const trimmedData = structuredClone(data)
   for (const key in trimmedData) {
